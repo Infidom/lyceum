ece9a7d57cc52c9d907700f16ea6ae85810763d6
Infidom <bodik3355@gmail.com>
2021-02-13 17:29:21 +0200

    Added HW

---
+++ week1/germanreich.py
@@ -0,0 +1,29 @@
+number_1 = 12
+number_2 = 13
+number_3 = 0
+
+del_1 = number_1 / number_2
+del_2 = number_2 / number_1
+del_3 = number_3 / number_2
+del_4 = number_1 / number_1
+del_5 = number_3 + number_3
+del_6 = number_1 + number_2
+del_7 = number_2 - number_3
+del_8 = number_1 - number_3
+del_9 = number_3 - number_2
+del_10 = number_1 * number_3
+del_11= number_2 * number_2
+del_12= number_1 * number_2
+
+print(del_1)
+print(del_2)
+print(del_3)
+print(del_4)
+print(del_5)
+print(del_6)
+print(del_7)
+print(del_8)
+print(del_9)
+print(del_10)
+print(del_11)
+print(del_12)
